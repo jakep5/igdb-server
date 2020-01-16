77109052583241546ad08e8d52ba7fb3d485ca9b
jakep5 <jakepagel1@yahoo.com>
2020-01-16 14:13:41 -0600

    fix whitelisted headers

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cors())
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, gameTitle, platformFilters, genreFilters, reviewFilter");
     next();
 });
 
